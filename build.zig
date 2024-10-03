3eedec9d1afac5ae66e3b988a33f178f1bcafe69
kennyfrc <fxkennyfrc@gmail.com>
2024-10-03 08:18:29 +0800

    fix: update root_source_file syntax in build.zig to use string path for Zig 0.13.0 compatibility

--- build.zig
+++ build.zig
@@ -6,7 +6,7 @@ pub fn build(b: *std.Build) void {
 
     const exe = b.addExecutable(.{
         .name = "sdl-zig-demo",
-        .root_source_file = .{ .path = "src/main.zig" },
+        .root_source_file = "src/main.zig",
         .target = target,
         .optimize = optimize,
     });
